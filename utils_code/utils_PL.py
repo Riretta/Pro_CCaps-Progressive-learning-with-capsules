9195f0225fcabe23bae1bafa4c4f6a3304f7b3df
Rita_Pucci <46971187+Riretta@users.noreply.github.com>
2021-10-13 12:10:42 +0200

    Add files via upload

--- utils_code/utils_PL.py
+++ utils_code/utils_PL.py
@@ -3,7 +3,12 @@ import torch.nn as nn
 from torch import Tensor
 import torch.nn.functional as F
 keys = ['primary','1','2','3','4','Q']
-layers_dim = {keys[0]:(15,15),keys[1]:(16,16),keys[2]:(20,20),keys[3]:(24,24),keys[4]:(28,28),keys[5]:(56,56)}
+layers_dim_224 = {keys[0]:(15,15),keys[1]:(16,16),keys[2]:(20,20),keys[3]:(24,24),keys[4]:(28,28),keys[5]:(56,56)}
+layers_dim_256 = {keys[0]:(19,19),keys[1]:(20,20),keys[2]:(24,24),keys[3]:(28,28),keys[4]:(32,32),keys[5]:(64,64)}
+layers_dim_ResNet = {keys[0]:(7,7),keys[1]:(7,7),keys[2]:(14,14),keys[3]:(28,28),keys[4]:(56,56),keys[5]:(56,56)}
+layers_dim_Zhang = {keys[0]:(27,27),keys[1]:(28,28),keys[2]:(28,28),keys[3]:(28,28),keys[4]:(28,28),keys[5]:(56,56)}
+layers_dim_UNET = {keys[1]:(44,44),keys[2]:(48,48),keys[3]:(52,52),keys[4]:(56,56),keys[5]:(56,56)}
+
 
 class Quantization_module(nn.Module):
     def __init__(self):
@@ -13,7 +18,17 @@ class Quantization_module(nn.Module):
         self.boost_layer = PriorBoostLayer()
         self.nongray_mask = NonGrayMaskLayer()
 
-    def forward(self,x,depth='Q'):
+    def forward(self,x,depth='Q',mode=224, ResNet_mode = False, Zhang_mode = False, UNET_mode = False):
+        if not ResNet_mode and not Zhang_mode and not UNET_mode:
+            if mode == 224: layers_dim = layers_dim_224
+            elif mode == 256: layers_dim = layers_dim_256
+        elif ResNet_mode:
+            layers_dim = layers_dim_ResNet
+        elif Zhang_mode:
+            layers_dim = layers_dim_Zhang
+        elif UNET_mode:
+            layers_dim = layers_dim_UNET
+
         xx = F.interpolate(x, size=layers_dim[depth])
         encode, max_encode = self.encode_layer.forward(xx)
 
